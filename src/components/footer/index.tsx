814ea3be3877aba307f9d0fd7dc6207afec0df4c
MagnoLevi <magnolevi2003@gmail.com>
2024-05-29 18:17:54 -0300

    Commit

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import './style.css';
 const Footer = () => {
     return(
         <footer>
-            <h6>Gabriel Cabral Darce Cardoso  |  João Henrique Medeiros de Lucena  |  Magno Levi dos Santos</h6>
+            <h6>Gabriel Cabral Darce Cardoso | Gabriel Lisboa |  João Henrique Medeiros de Lucena  |  Magno Levi dos Santos</h6>
             <h6>Sistemas para Internet - P4</h6>
         </footer>
     )
